38733b5e90d6f3f3094da95f56d90a24bf54d4ad
Nishant <nishant203@gmail.com>
2021-01-02 22:24:49 +0530

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import MarketingApp from './components/MarketingApp'
+export default () => {
+    return <div>
+        Hi there
+        <hr />
+        <MarketingApp></MarketingApp>
+    </div>
+}
